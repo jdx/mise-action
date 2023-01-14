168c20f963c82cca27995c6f92d398fd6a6a519b
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2023-01-14 08:44:01 -0600

    dist

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,15 @@ async function run(): Promise<void> {
 }
 
 async function setupRTX(): Promise<void> {
-  console.error("TODO: SETUPRTX");
+  core.debug('Not implemented')
 }
 
 async function setToolVersions(): Promise<void> {
-  let toolVersions = core.getInput("tool_versions", { required: false });
+  const toolVersions = core.getInput('tool_versions', {required: false})
   if (toolVersions) {
-    await fs.promises.writeFile(".tool-versions", toolVersions, {
-      encoding: "utf8",
-    });
+    await fs.promises.writeFile('.tool-versions', toolVersions, {
+      encoding: 'utf8'
+    })
   }
 }
 
@@ -31,4 +31,4 @@ if (require.main === module) {
   }
 }
 
-export { run }
+export {run}
