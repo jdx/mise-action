3600b6410713f6187847c1eb2bde38315e844484
jdx <216188+jdx@users.noreply.github.com>
2025-08-18 11:51:16 -0500

    chore: fix release-plz

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,10 +2,10 @@
 set -euxo pipefail
 
 function assert_equal() {
-  if [ "$1" != "$2" ]; then
-    echo "Assertion failed: Expected '$1', got '$2'" >&2
-    return 1
-  fi
+	if [ "$1" != "$2" ]; then
+		echo "Assertion failed: Expected '$1', got '$2'" >&2
+		return 1
+	fi
 }
 EXPECTED_OUTPUT="jq-1.7.1"
 
@@ -14,7 +14,7 @@ which jq
 
 # windows bash does not seem to work with shims
 if [[ "$(uname)" != "MINGW"* ]]; then
-  assert_equal "$EXPECTED_OUTPUT" "$(jq --version)"
+	assert_equal "$EXPECTED_OUTPUT" "$(jq --version)"
 fi
 
 # checking that environment variables set in mise.toml are properly set
